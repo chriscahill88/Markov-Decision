eb9664485d4565d5cf615ca46403ee083a5bd5ad
chriscahill88 <43705092+chriscahill88@users.noreply.github.com>
2023-12-03 20:24:46 -0500

    Update policyIteration.py

--- policyIteration.py
+++ policyIteration.py
@@ -1,12 +1,11 @@
-#This code was written by ChatGPT 3.5 on December 3rd at 2:25 P.M and was given the prompt "can you give me the code for policy iteration given the same grid as value iterations"
 import numpy as np
 
 # Initial set up
 
 # Hyperparameters
-SMALL_ENOUGH = 0.05
-GAMMA = 0.95
-NOISE = 0.0
+SMALL_ENOUGH = 0.005
+GAMMA = 0.9
+NOISE = 0.1
 
 # Define all states
 all_states = []
@@ -21,131 +20,90 @@ for i in all_states:
         rewards[i] = -1700
     elif i == (1, 4):
         rewards[i] = 1700
-    #elif i == (3, 0):
-        #rewards[i] = 20
+   # elif i == (3, 0):
+    #    rewards[i] = 20
     #elif i == (4, 5):
-        #rewards[i] = -20
+    #    rewards[i] = -20
     #elif i == (2, 0):
-        #rewards[i] = -5
+    #    rewards[i] = -5
     #elif i == (0, 1):
-        #rewards[i] = 10
+    #    rewards[i] = 10
     else:
         rewards[i] = 0
 
-# Define initial state (starting position of the character)
-initial_state = (5, 0)
-
-# Dictionary of possible actions
-actions = {
-    (0, 0): ('D', 'R'),
-    (0, 1): ('D', 'R', 'L'),
-    (0, 2): ('D', 'L', 'R'),
-    (0, 3): ('D', 'L', 'R'), 
-    (0, 4): ('D', 'L', 'R'),  
-    (0, 5): ('D', 'L'),  
-    (1, 0): ('D', 'U', 'R'),
-    (1, 1): ('D', 'R', 'L', 'U'),
-    (1, 2): ('D', 'R', 'L', 'U'),
-    (1, 3): ('D', 'R', 'L', 'U'),
-    (1, 4): ('D', 'R', 'L', 'U'),
-    (1, 5): ('D', 'L', 'U'),
-    (2, 0): ('D', 'U', 'R'),
-    (2, 1): ('D', 'U', 'L', 'R'),
-    (2, 2): ('D', 'U', 'L', 'R'),
-    (2, 3): ('D', 'U', 'L', 'R'),
-    (2, 4): ('D', 'U', 'R', 'L'),
-    (2, 5): ('D', 'U', 'L'),  
-    (3, 0): ('D', 'U', 'R'),
-    (3, 1): ('D', 'R', 'L', 'U'),
-    (3, 2): ('D', 'R', 'L', 'U'),
-    (3, 3): ('D', 'U', 'R', 'L'),
-    (3, 4): ('D', 'U', 'R', 'L'),
-    (3, 5): ('D', 'U', 'L'),
-    (4, 0): ('D', 'U', 'R'),
-    (4, 1): ('D', 'R', 'L', 'U'),
-    (4, 2): ('D', 'R', 'L', 'U'),
-    (4, 3): ('D', 'U', 'R', 'L'),
-    (4, 4): ('D', 'U', 'R', 'L'),
-    (4, 5): ('D', 'U', 'L'),
-    (5, 0): ('U', 'R'),
-    (5, 1): ('U', 'L', 'R'),
-    (5, 2): ('U', 'L', 'R'),  
-    (5, 3): ('R', 'L', 'U'),
-    (5, 4): ('R', 'L', 'U'),
-    (5, 5): ('L', 'U'),
-}
-
-# Define an initial policy and value function
-policy = {state: np.random.choice(actions[state]) for state in actions.keys()}
+# Define initial policy and value function
+policy = {state: np.random.choice(['U', 'D', 'L', 'R']) for state in all_states}
 V = {state: 0 for state in all_states}
 
-# Set the initial state and value
-policy[initial_state] = 'U'
-V[initial_state] = 0
+# Policy Iteration
 
-def get_next_state(state, action):
-    if action == 'U':
-        return max(0, state[0] - 1), state[1]
-    elif action == 'D':
-        return min(5, state[0] + 1), state[1]
-    elif action == 'L':
-        return state[0], max(0, state[1] - 1)
-    elif action == 'R':
-        return state[0], min(5, state[1] + 1)
-
-def reverse_action(action):
-    if action == 'U':
-        return 'D'
-    elif action == 'D':
-        return 'U'
-    elif action == 'L':
-        return 'R'
-    elif action == 'R':
-        return 'L'
-
-# Value Iteration
 max_iterations = 100
 iteration = 0
-
 while True:
-    biggest_change = 0
-    for state in all_states:
-        if state in policy:
+    # Policy Evaluation
+    while True:
+        biggest_change = 0
+        for state in all_states:
             old_v = V[state]
-            new_v = float('-inf')
-            best_action = None
 
+            action = policy[state]
+            if action == 'U':
+                next_state = (max(0, state[0] - 1), state[1])
+            elif action == 'D':
+                next_state = (min(5, state[0] + 1), state[1])
+            elif action == 'L':
+                next_state = (state[0], max(0, state[1] - 1))
+            elif action == 'R':
+                next_state = (state[0], min(5, state[1] + 1))
 
-
-            for action in actions[state]:
-                next_state = get_next_state(state, action)
-                # Calculate the value
-                value = rewards[state] + (GAMMA * ((1 - NOISE) * V[next_state] + (NOISE * V[state])))
-                if value > new_v:  # Is this the best action so far? If so, keep it
-                    new_v = value
-                    best_action = action
-
-            # Save the best action for the state
-            V[state] = new_v
-            policy[state] = best_action
+            next_state = tuple(next_state)
+            V[state] = rewards[state] + (GAMMA * ((1 - NOISE) * V[next_state] + (NOISE * V[state])))
             biggest_change = max(biggest_change, np.abs(old_v - V[state]))
 
+        if biggest_change < SMALL_ENOUGH:
+            break
 
-    # See if the loop should stop now
-    if biggest_change < SMALL_ENOUGH:
-        print(f"Converged after {iteration} iterations due to SMALL_ENOUGH")
+    # Policy Improvement
+    policy_stable = True
+    for state in all_states:
+        old_action = policy[state]
+
+        # Find the action that maximizes expected value
+        values = []
+        for action in ['U', 'D', 'L', 'R']:
+            if action == 'U':
+                next_state = (max(0, state[0] - 1), state[1])
+            elif action == 'D':
+                next_state = (min(5, state[0] + 1), state[1])
+            elif action == 'L':
+                next_state = (state[0], max(0, state[1] - 1))
+            elif action == 'R':
+                next_state = (state[0], min(5, state[1] + 1))
+
+            next_state = tuple(next_state)
+            value = rewards[state] + (GAMMA * ((1 - NOISE) * V[next_state] + (NOISE * V[state])))
+            values.append(value)
+
+        best_action = ['U', 'D', 'L', 'R'][np.argmax(values)]
+        policy[state] = best_action
+
+        if old_action != best_action:
+            policy_stable = False
+
+    if policy_stable:
+        print(f"Converged after {iteration} iterations")
         break
+
     iteration += 1
     if iteration == max_iterations:
-        print(f"Maximum number of iterations {iteration} reached.")
-        break
+        print("Maximum number of iterations reached.")
 
 # Print the optimal path with random decisions
 print("\nOptimal Path:")
-current_state = initial_state
+current_state = (5, 0)  # Starting state
 while True:
     print(f"Current State: {current_state}, Action: {policy[current_state]}")
-    
+
     # Make random decisions
     rand_val = np.random.rand()
     if rand_val < 0.15:
@@ -153,11 +111,17 @@ while True:
     elif rand_val < 0.3:
         # Move in reverse
         action = policy[current_state]
-        next_state = get_next_state(current_state, reverse_action(action))
+        next_state = (max(0, current_state[0] - 1), current_state[1]) if action == 'U' else \
+                     (min(5, current_state[0] + 1), current_state[1]) if action == 'D' else \
+                     (current_state[0], max(0, current_state[1] - 1)) if action == 'L' else \
+                     (current_state[0], min(5, current_state[1] + 1))
     elif rand_val >= 0.3:
         # Move forward
         action = policy[current_state]
-        next_state = get_next_state(current_state, action)
+        next_state = (max(0, current_state[0] - 1), current_state[1]) if action == 'U' else \
+                     (min(5, current_state[0] + 1), current_state[1]) if action == 'D' else \
+                     (current_state[0], max(0, current_state[1] - 1)) if action == 'L' else \
+                     (current_state[0], min(5, current_state[1] + 1))
 
     current_state = next_state
 
@@ -165,6 +129,7 @@ while True:
     if current_state == (1, 4):
         print(f"Reached the goal state: {current_state}")
         break
+
 # Display the results
 print("Optimal Value Function:")
 for i in range(6):
@@ -194,4 +159,3 @@ for i in range(6):
         else:
             row += "N/A\t"
     print(row)
-
